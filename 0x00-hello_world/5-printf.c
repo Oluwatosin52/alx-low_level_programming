66bfe7958746abcd44229465812f722b88db6867
Azeez Israel <you@example.com>
2022-09-08 15:16:04 -0700

    solution to task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,12 @@
-#iclude <stdio.h>
+#include <stdio.h>
 
-exit
+/**
+ * main - Entry point
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0)
+}
