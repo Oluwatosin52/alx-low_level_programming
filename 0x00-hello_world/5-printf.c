ebafb608474b7ac65850e892aeee4ffaa7f5b934
Azeez Israel <you@example.com>
2022-09-08 15:00:00 -0700

    solution to task 4

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,3 @@
-/**
- * main - print the string to the put function
- *
- * Description: using the main function
- * this program prints "with proper grammar, but the outcome is a piece of art,
- * Return: 0
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n*);
-	return (0);
-}
+#iclude <stdio.h>
+
+exit
