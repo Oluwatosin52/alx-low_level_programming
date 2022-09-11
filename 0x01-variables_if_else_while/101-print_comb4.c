4a0fcf4ecf3ffafcaab5bfe784408bb933b094a2
Azeez Israel <you@example.com>
2022-09-11 12:12:30 -0700

    solution to task 10

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 /**
- * main - program that prints all possible different combinations of two digits
+ * main - program that prints all possible different combinations of 3 digits
  * Return: 0
  */
 int main(void)
 {
 	int c = 0;
 	int f_d;
+	int m_d;
 	int l_d;
 
-	while (c <= 99)
+	while (c <= 999)
 	{
-		f_d = (c / 10 + '0');
+		f_d = (c / 100 + '0');
+		m_d = (c / 10 % 10 + '0');
 		l_d = (c % 10 + '0');
 
-		if (f_d < l_d)
+		if ((f_d < m_d) && (m_d < l_d))
 		{
 			putchar(f_d);
+			putchar(m_d);
 			putchar(l_d);
 
-			if (c != 89)
+			if (c != 789)
 			{
 				putchar(',');
 				putchar(' ');
