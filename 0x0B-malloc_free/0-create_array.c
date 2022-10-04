a1d386c11bb62b2fe9d1c7864a7ba153e90bde54
Azeez Israel <you@example.com>
2022-10-04 11:11:24 -0700

    solution to task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * malloc_checked - This provides a safe way of mallocing
  * @b: This is the number of bytes to be malloced
@@ -11,7 +12,6 @@ void *malloc_checked(unsigned int b)
 	void *ptr;
 
 	ptr = malloc(b);
-
 	if (ptr == NULL)
 		exit(98);
 	return (ptr);
